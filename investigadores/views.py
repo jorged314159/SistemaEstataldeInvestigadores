c8929fa46142c3a79b69eeccc401235b45f85fea
Alfonso Rafael Solis Rangel <alfonsorafaelsolisrangel@gmail.com>
2023-11-24 11:56:02 -0600

    fixed bug

--- investigadores/views.py
+++ investigadores/views.py
@@ -95,8 +95,6 @@ class SolicitudCategoriaA(LoginRequiredMixin, CreateView):
         userid = self.request.user.id
         
         context["c1"] = CategoriaA.objects.filter(user_id = userid, anio=datetime.datetime.today().year).count()
-        CatA = CategoriaA.objects.filter(user_id = userid, anio=datetime.datetime.today().year).first()
-        context["c2"] = CatA.pk;
         return context
 
     def form_valid(self, form):
@@ -326,12 +324,15 @@ class EditarRevisionCatB(LoginRequiredMixin, UpdateView):
 def premio_estatal_cyt(request):
     userid = request.user.id
     countCatA = CategoriaA.objects.filter(user_id = userid, anio=datetime.datetime.today().year).count()
+    print(countCatA)
     if(countCatA == 0):
         return redirect("investigadores:premios-categoria-a")
     else:
-        catA = CategoriaA.objects.filter(user_id = userid, anio=datetime.datetime.today().year).first()
-        return redirect("investigadores:update-premios-categoria-a", catA.pk)
-
+        try:
+            catA = CategoriaA.objects.filter(user_id = userid, anio=datetime.datetime.today().year).first()
+            return redirect("investigadores:update-premios-categoria-a", catA.pk)
+        except:
+            print("Error ocurred")
 @login_required
 def investigaciones_google(request):
     if request.method == "POST":
